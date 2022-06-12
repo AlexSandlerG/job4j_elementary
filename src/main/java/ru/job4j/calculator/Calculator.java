8ea0a4a979592b32ea3ffaf863399551848ce9fa
AlexSandlerG <alex-sandler_java@mail.ru>
2022-06-12 17:13:13 +0300

    3. Арифметические операции. [#185 #42009]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,25 +1,8 @@
 package ru.job4j.calculator;
 
-import static ru.job4j.math.MathFunction.*;
-
 public class Calculator {
 
-    private static int x = 5;
-
-    public static int minus(int y) {
-        return x - y;
-    }
-
-    public int divide(int y) {
-        return y / x;
-    }
-
-    public double sumAllOperation(int y) {
-        return minus(y) + divide(y) + sum(y, x) + multiple(y, x);
-    }
-
     public static void main(String[] args) {
-/*
         int one = 1;
         int two = 2;
         int four = 4;
@@ -33,12 +16,5 @@ public class Calculator {
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
- */
-        int rslMinus = minus(8);
-        Calculator calculatorDivide = new Calculator();
-        int rslDivide = calculatorDivide.divide(10);
-        double rslSumAll = calculatorDivide.sumAllOperation(6);
-        System.out.println(rslMinus + System.lineSeparator()
-                + rslDivide + System.lineSeparator() + rslSumAll);
     }
 }
